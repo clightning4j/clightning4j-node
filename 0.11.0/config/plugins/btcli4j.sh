87bac660f631ce440158d8c568a87767a2528624
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-29 23:01:35 +0200

    img: add clightning4j-node 0.11.0
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ 0.11.0/config/plugins/btcli4j.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec java -jar /opt/btcli4j-all.jar
---
+++ 0.11.0/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo $PWD
+
+set -e
+
+tor -f /opt/torrc --runasdaemon 1 --quiet
+
+curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs
+
+chown -R clightning4j /home/clightning4j/.tor
+chown -R clightning4j /opt
+chown clightning4j /conf_env.vsh
+
+./conf_env.vsh "$@"
