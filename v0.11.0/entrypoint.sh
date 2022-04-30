d50207a111f7e6b029b631d9baddf401b8f34dd6
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-30 09:40:23 +0200

    ci: report the failure inside docker to the ci
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- v0.11.0/entrypoint.sh
+++ v0.11.0/entrypoint.sh
@@ -9,7 +9,7 @@ curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.t
 
 chown -R clightning4j /home/clightning4j/.tor
 chown -R clightning4j /opt
-chown clightning4j /conf_env.vsh
+chown clightning4j /home/clightning4j/conf_env.vsh
 
 cd /home/clightning4j
 ./conf_env.vsh "$@"
