7d9fa26175215a172369e778a42034d14749b11d
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-30 20:23:23 +0200

    img: introduce lnmetrics plugin
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- v0.11.0/entrypoint.sh
+++ v0.11.0/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 chown -R tor:nogroup /var/lib/tor
-#chown -R clightning4j /opt
-#chown clightning4j /opt/conf_env.vsh
-#chown clightning4j /opt/go-lnmetrics
+chown root /opt/go-lnmetrics
 
 v run /opt/conf_env.vsh "$@"
