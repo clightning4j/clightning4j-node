e09345b5dc25363bdd0893b766ec7c232b2420de
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-30 19:29:51 +0200

    imgs: fixed build and restart of the node
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- v0.11.0/entrypoint.sh
+++ v0.11.0/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-echo $PWD
-
 set -e
+chown -R tor:nogroup /var/lib/tor
+#chown -R clightning4j /opt
+#chown clightning4j /opt/conf_env.vsh
+#chown clightning4j /opt/go-lnmetrics
 
-tor -f /opt/torrc --runasdaemon 1 --quiet
-
-curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs
-
-chown -R clightning4j /home/clightning4j/.tor
-chown -R clightning4j /opt
-chown clightning4j /home/clightning4j/conf_env.vsh
-
-cd /home/clightning4j
-./conf_env.vsh "$@"
+v run /opt/conf_env.vsh "$@"
