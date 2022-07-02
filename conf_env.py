a4d7dde9d327926f03c352fc8166672a8cb90999
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-02 17:51:48 +0000

    docker: fixing port

--- conf_env.py
+++ conf_env.py
@@ -2,39 +2,60 @@
 import subprocess
 import os
 import sys
+import fnmatch
+import shutil
 from os import path
 
-def run_cmd(cmd: str) -> str:
-	process = subprocess.run(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-    check=True, text=True)
-	print(process.stdout)
+
+def run_cmd(cmd: str, shell: bool = False):
+    process = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+    print(process.stdout.decode("utf-8"))
+    return process.returncode
+
+
+def copyfiles(srcdir: str, dstdir: str, filepattern: str):
+    def failed(exc):
+        raise exc
+
+    for dirpath, dirs, files in os.walk(srcdir, topdown=True, onerror=failed):
+        for file in fnmatch.filter(files, filepattern):
+            shutil.copy2(os.path.join(dirpath, file), dstdir)
+
 
 def run_tor():
-	if path.exists('/home/clightning4j/.tor'):
-		run_cmd('rm -r /home/clightning4j/.tor')
+    if path.exists("/home/clightning4j/.tor"):
+        run_cmd("rm -r /home/clightning4j/.tor")
 
-	run_cmd('tor --runasdaemon 1')
-	run_cmd('curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs')
-	run_cmd('chown -R clightning4j /home/clightning4j/.tor')
+    run_cmd("tor --runasdaemon 1")
+    run_cmd(
+        "curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs"
+    )
+    run_cmd("chown -R clightning4j /home/clightning4j/.tor")
 
 
 def build_ln_directory() -> str:
-	ln_dir = os.environ['CLIGHTNING_DATA']
-	if not path.exists(ln_dir):
-		run_cmd(f"mkdir {ln_dir}")
-		run_cmd('cp /opt/config $ln_dir/config')
-		run_cmd('mkdir $ln_dir/plugins')
-		run_cmd('cp /opt/*.sh $ln_dir/plugins/')
-		run_cmd('chown -R clightning4j $ln_dir')
-	run_tor()
-	return ln_dir
+    ln_dir = os.environ["CLIGHTNING_DATA"]
+    if not path.exists(ln_dir):
+        run_cmd(f"mkdir {ln_dir}")
+        run_cmd(f"cp -rf /opt/config {ln_dir}/config")
+        run_cmd(f"mkdir {ln_dir}/plugins")
+        copyfiles("/opt", f"{ln_dir}/plugins", "*.sh")
+        run_cmd(f"chown -R clightning4j {ln_dir}")
+    run_tor()
+    return ln_dir
 
-args_str = ''
-for arg in sys.argv:
-	args_str += arg + ' '
 
-if 'lightning-cli' in args_str or not '--' in args_str:
-	run_cmd(args_str)
-else:
-	ln_dir = build_ln_directory()
-	run_cmd(f'lightningd --lightning-dir={ln_dir} {args_st}')
+if __name__ == "__main__":
+    args_str = ""
+    for arg in sys.argv:
+        if arg == "/opt/conf_env.py":
+            continue
+        args_str += arg + " "
+
+    if "lightning-cli" in args_str or not "--" in args_str:
+        run_cmd(args_str)
+    else:
+        ln_dir = build_ln_directory()
+        cmd = f"lightningd --lightning-dir={ln_dir} {args_str}"
+        print(f"cmd prepared is: {cmd}")
+        sys.exit(run_cmd(cmd, shell=True))
